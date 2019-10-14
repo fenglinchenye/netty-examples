57dd85b04f2008771b0514cfa210b00b352b0332
fenglinchenye <905584636@qq.com>
2019-10-14 22:14:44 +0800

    netty使用demo示例代码

---
+++ src/main/java/com/freedo/example/server/TimeServer.java
@@ -0,0 +1,56 @@
+package com.freedo.example.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+
+    public void bind(int port) throws Exception {
+        // 配置服务端的NIO线程组
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup,workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,1024)
+                    .childHandler(new ChildChannelHandler());
+
+            //绑定端口,同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+
+            //等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 优雅的推出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args!=null&&args.length>0){
+         try{
+             port = Integer.valueOf(args[0]);
+         }catch (NumberFormatException e){
+             //采用默认值
+         }
+        }
+        new TimeServer().bind(port);
+    }
+
+}
+
+class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        socketChannel.pipeline().addLast(new TimeServerHandler());
+    }
+}
+
---
+++ src/main/java/com/freedo/example/server/handler/TimeServerHandler.java
@@ -0,0 +1,10 @@
+package com.freedo.example.server.handler;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+public class TimeServerHandler extends ChannelHandlerAdapter {
+
+
+
+
+}
